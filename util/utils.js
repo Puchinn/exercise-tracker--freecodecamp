0ed9663a24bd45e396b8d470e8cf34a7d04eaf21
Esteban Sayago <sayagoo823@gmail.com>
2023-12-27 16:33:58 -0300

    add validation to invalid dates

--- util/utils.js
+++ util/utils.js
@@ -2,6 +2,9 @@ function checkDate(date) {
   if (date === "" || date === null || date === undefined) {
     return new Date().toDateString();
   }
+  if (new Date(date) === "Invalid Date") {
+    return new Date().toDateString();
+  }
   return new Date(date).toDateString();
 }
 
