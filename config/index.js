166aa766de7bf713fd6fa5bf04ce37bcdd9a3bce
Esteban Sayago <sayagoo823@gmail.com>
2023-12-27 17:14:51 -0300

    add config for mongoose connection options

--- config/index.js
+++ config/index.js
@@ -1,11 +1,14 @@
 require("dotenv").config();
 
-const { MONGODB_URL, PORT } = process.env;
+const { MONGODB_URL, PORT, DB_NAME } = process.env;
 const MONGODB_URL_TEST = "mongodb://localhost:27017";
 
 const config = {
   db: {
-    mongo: MONGODB_URL || MONGODB_URL_TEST,
+    mongo_url: MONGODB_URL || MONGODB_URL_TEST,
+    options: {
+      dbName: DB_NAME || "test",
+    },
   },
   app: {
     port: PORT || 3000,
