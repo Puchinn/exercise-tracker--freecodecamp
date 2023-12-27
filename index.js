422fc1326f364e6ee2c5017866468f56a0a02be3
Esteban Sayago <sayagoo823@gmail.com>
2023-12-27 17:15:48 -0300

    use mongoose connection options config

--- index.js
+++ index.js
@@ -7,24 +7,20 @@ const config = require("./config/index");
 const app = express();
 const appRoutes = require("./routes/index.js");
 
-mongoose
-  .connect(config.db.mongo, {
-    dbName: "test",
-  })
-  .then(() => {
-    console.log("connection to database sucesfull!");
-    app.use(cors());
-    app.use(bodyParser.urlencoded({ extended: false }));
-    app.use(bodyParser.json());
-    app.use(express.static("public"));
+mongoose.connect(config.db.mongo_url, config.db.options).then(() => {
+  console.log("connection to database sucesfull!");
+  app.use(cors());
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(bodyParser.json());
+  app.use(express.static("public"));
 
-    app.use("/", appRoutes);
+  app.use("/", appRoutes);
 
-    app.get("/", (req, res) => {
-      res.sendFile(`${__dirname}/views/index.html`);
-    });
-
-    app.listen(config.app.port, () => {
-      console.log("Server is running at port: ", config.app.port);
-    });
+  app.get("/", (req, res) => {
+    res.sendFile(`${__dirname}/views/index.html`);
   });
+
+  app.listen(config.app.port, () => {
+    console.log("Server is running at port: ", config.app.port);
+  });
+});
